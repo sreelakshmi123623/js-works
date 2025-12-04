9a2fcf5b1aac04412bb85706643b8ed4bc7a397f
sreelakshmi123623 <sreel5148@gmail.com.com>
2025-12-04 11:03:13 +0530

    modified

--- task.js/characterTypeChecker.js
+++ task.js/characterTypeChecker.js
@@ -1,1 +1,17 @@
 //check if input is a letter ,number or special charatter
+
+
+ var input = "500";
+
+ if (input>="a" && input<="z" || input>="A" && input<="Z") {
+    console.log(`${input} is a letter`);
+    
+ }
+  else if(input>=0 && input<=1001) {
+        console.log(`${input} is a number`);
+        
+    }
+    else{
+        console.log(`${input} is a special character`);
+        
+    }
